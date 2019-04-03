f14e3b392e83493a3d2ad70e8f0405787fe2315c
Shane Weisz <shaneweisz@gmail.com>
2019-04-03 10:21:27 +0200

    Adjusted FIFO algorithm with better practices

--- paging.py
+++ paging.py
@@ -5,42 +5,50 @@ import sys
 from random import randint
 
 
+# Function that returns the number of page faults using FIFO
 def FIFO(size, pages):
-    # size = number of pages/frames
-    # pages = page reference string
-    # returns the number of faults using FIFO
+    # size:  number of available frames
+    # pages: page reference string
 
     queue = []                   # queue is initially empty
     page_faults = 0              # tracks the number of page faults
 
-    for page in pages:           # loop through each page in the page ref str
-        if page not in queue:
-            page_faults += 1     # a page fault has occurred
-            del queue[0]         # remove page at head of queue
-            queue.append(page)   # append the new page at end of queue
+    for page in pages:           # loops through each page in the page ref str
+        if len(queue) < size:    # check if the queue is full
+            if page not in queue:
+                page_faults += 1     # a page fault has occurred
+                queue.append(page)   # append the new page at end of queue
+        else:                    # the queue is now full
+            if page not in queue:
+                page_faults += 1     # a page fault has occurred
+                del queue[0]         # remove page at head of queue
+                queue.append(page)   # append the new page at end of queue
 
     return page_faults
 
 
+# Function that returns the number of page faults using LRU
 def LRU(size, pages):
-    # size = number of pages
+    # size:  number of available frames
+    # pages: page reference string
     pass
 
 
+# Function that returns the number of page faults using OPT
 def OPT(size, pages):
-    # size = number of pages
+    # size:  number of available frames
+    # pages: page reference string
     pass
 
 
 def main():
     # Generates a random page-reference string where page nos range from 0 to 9
     pages = []                           # The page-reference string itself
-    for i in range(20):                  # Assume 20 pages in pag
+    for i in range(20):                  # Assume 20 pages in page ref str
         pages.append(randint(0, 9))      # Generates a random number from [0,9]
 
     # pages = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1]
-    pages = [1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5]
-    print pages
+    # pages = [1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5]
 
     # Apply the random page-reference string to each algorithm,
     # and record the number of page faults incurred by each algorithm.
